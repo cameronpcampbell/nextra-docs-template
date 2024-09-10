c0996f9da860eecca992b55650c12a8663b33fe7
Cameron Campbell <cameroncampbell@Camerons-MacBook-Pro.local>
2024-09-10 03:34:14 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,2 +1,2 @@
 const withNextra = require("nextra")({ theme: "nextra-theme-docs", themeConfig: "./theme.config.tsx" })
-module.exports = withNextra({ output: "export", distDir: "./build", basePath: "", images: { loader: "akamai", path: "" } })
+module.exports = withNextra({ output: "export", distDir: "./build", basePath: "", images: { loader: "akamai", path: "", unoptimized: true }, })
